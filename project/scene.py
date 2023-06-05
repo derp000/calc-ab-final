3fd335ddd3f6b9ecb23b067efef7727160e2f93e
derp000 <84947701+derp000@users.noreply.github.com>
2023-06-05 17:35:05 -0400

    Rendered some stuff

--- project/scene.py
+++ project/scene.py
@@ -12,6 +12,9 @@ def fz(z):
 
 class MainView(ThreeDScene):
     def construct(self):
+        """
+        First, let's orient ourselves in 3D.
+        """
         self.next_section("Graph Setup", skip_animations=True)
 
         # graph setup
@@ -24,11 +27,6 @@ class MainView(ThreeDScene):
             "z_length": 7,
         }
         axes = ThreeDAxes(**axis_config)
-        # ax = ThreeDAxes()
-        # x_labels = range(-4, 5)
-        # z_labels = range(-4, 4, 2)
-        # ax.add_coordinates(x_labels, None, z_labels)  # default y labels, custom x & z labels
-        # ax.add_coordinates(x_labels)  # only x labels
         x_axis = axes.get_x_axis()
         x_label = axes.get_x_axis_label("x")
         y_axis = axes.get_y_axis()
@@ -36,11 +34,6 @@ class MainView(ThreeDScene):
         z_axis = axes.get_z_axis()
         z_label = axes.get_z_axis_label("z")
 
-        # axes.add_coordinates(
-        #     np.linspace(-1, 1, 10, endpoint=False),
-        #     np.linspace(-1, 1, 10, endpoint=False),
-        #     range(0),
-        # )
         graph = VGroup(x_axis, y_axis, z_axis, x_label, y_label, z_label)
         self.add(graph)
 
@@ -102,25 +95,19 @@ class MainView(ThreeDScene):
                 Write(z_label),
             ],
         )
-        self.wait()
+        self.wait(2.5)
 
         self.next_section("Function Visualization", skip_animations=True)
 
         """
             Note that the base of the tent is a square. This square can be defined by two identical 
-            parametric curves, with one in the XZ plane and the other in the YZ plane. If we were able
-            to get every square cross section with respect to the z-axis, then we would be able to get
-            the volume of the tent. In other words, we want to integrate some function A(z) from z=0
-            to z=tent_height such that A(z) gets us the area of the square cross section at some 
-            value z.
-            
+            parametric curves, with one in the XZ plane and the other in the YZ plane. These curves
+            are the metal frame of the tent.
         """
 
-        self.play(Create(x_graph), Create(y_graph))
+        self.wait(2.5)
+        self.play(Create(x_graph), Create(y_graph), run_time=2.0)
         self.begin_ambient_camera_rotation(0.1)
-        self.wait(5)
-
-        self.next_section("Cross Section Visualization", skip_animations=True)
 
         z = ValueTracker(0)
 
@@ -155,21 +142,31 @@ class MainView(ThreeDScene):
         )
         sqr_sec = VGroup(quad_one, quad_two, quad_three, quad_four)
 
-        self.play(Create(sqr_sec, lag_ratio=0.0))
-        self.wait(2.0)
-        self.play(z.animate.set_value(Z_INT), run_time=5.0)
-        self.play(FadeOut(sqr_sec))
-        self.wait(5)
+        self.play(Write(sqr_sec, lag_ratio=0.0), run_time=2.0)
+        self.wait(20.0)
 
-        self.next_section("XY", skip_animations=True)
+        self.next_section("Cross Section Visualization", skip_animations=True)
+
+        """
+            If we were able to get the relationship between z and the area of each square cross section, 
+            then we would be able to get the volume of the tent. In other words, we want to integrate some 
+            function A(z) from z=0 to z=tent_height such that A(z) gets us the area of the square cross section 
+            at some value z.
+        """
+
+        self.wait(2.5)
+
+        self.play(z.animate.set_value(Z_INT), run_time=15.0)
+        self.play(FadeOut(sqr_sec))
+
+        self.wait(2.5)
+
+        # ~~~~ RENDER AS ONE SECTION AND SPLICE AT EACH MULTILINE COMMENT ~~~~~
+        self.next_section("XY", skip_animations=False)
 
         """
             Well, how can we do that? Let's first look at each square cross section from the top down,
-            such that we will be looking at the XY plane. Let's draw in a line r from the
-            center of the square to one of its corners such that r is the square's radius. Now, let's
-            play the animation from before, increasing z from 0 to the tent height. Notice the clear
-            relationship between r and z: they are inversely proportional. Why is this useful? Well
-            that's because we can directly relate the square's radius to its area! {go to TopView}
+            such that we will be looking at the XY plane.
         """
 
         # showing radius in top view
@@ -205,6 +202,9 @@ class MainView(ThreeDScene):
         self.add_fixed_orientation_mobjects(r_val_obj, z_val_obj)
         top_render = VGroup(dot, r_line, r_obj, r_val_obj, z_val_obj)
 
+        # START ANIM
+        self.wait(2.5)
+
         self.stop_ambient_camera_rotation()
         self.move_camera(
             phi=0 * DEGREES,
@@ -216,28 +216,49 @@ class MainView(ThreeDScene):
             ],
             zoom=0.6,
         )
+        self.wait(2.5)
         self.play(z.animate.set_value(0), run_time=0.1)
-        self.wait(0.1)
-        self.play(Write(sqr_sec), run_time=2.0)
+
+        """
+            Let's draw in a line r from the center of the square to one of its corners such that r is the 
+            square's radius. 
+        """
+
         self.wait()
-        self.play(Create(top_render))
+        self.play(Create(r_line))
+        self.play(Create(r_obj))
+        self.wait()
+
+        """
+            Now, before we play the animation again, let's keep track of the value of r and 
+            z as z increases.
+        """
+
+        self.wait()
+        self.play(Create(sqr_sec), run_time=5.0)
+        self.play(Create(dot), run_time=2.0)
+        self.wait()
+        self.play(Create(r_val_obj))
+        self.play(Create(z_val_obj))
         self.wait(5)
 
-        self.next_section("XY change z", skip_animations=True)
-
         """
-            {Transition to TopView}
+            Notice the clear relationship between r and z: they are inversely proportional. 
+            Why is this useful? Well that's because we can directly relate the square's radius 
+            to its area!  
+            {Transition to TopView after this section}
         """
 
-        self.play(z.animate.set_value(Z_INT), run_time=3.0)
+        self.play(z.animate.set_value(Z_INT), run_time=15.0)
 
         self.next_section("XZ", skip_animations=True)
 
         """
-            Again, we want the relationship, or in other words, a function relating z and the area, 
-            since we are basically integrating A(z) = 2 * r(z)^2. How can we find this function? Well 
-            first, let's orient ourselves on the XZ plane, such that we are looking at parametric 
-            function on the XZ plane. 
+            {show slides}
+            Again, we want the relationship, or in other words, a function, relating z and the area, 
+            since we are basically integrating A(z) = 2 * r(z)^2. How can we find this function? Right now, 
+            we only know the relationship between radius and area. Well first, let's orient ourselves on 
+            the XZ plane, such that we are looking at parametric function on the XZ plane. 
             {go to SideView}
         """
 
@@ -257,11 +278,13 @@ class MainView(ThreeDScene):
         )
         self.wait(5)
 
-        self.next_section("Complete 3D Visualization")
+        self.next_section("Complete 3D Visualization", skip_animations=True)
 
         """
             If we go back into 3D, we can see all these components in play. Notice that as the value of
             z increases, r decreases and thus area decreases as well, which our new equation now models.
+            Now that we finally have our equation relating z to cross section area, we can now integrate it.
+            {go to AreaGraph}
         """
 
         self.play(z.animate.set_value(0), run_time=0.1)
@@ -272,17 +295,6 @@ class MainView(ThreeDScene):
                 axes.c2p(fz(z.get_value()), 0, z.get_value())
             ),
         )
-        # area_obj = always_redraw(
-        #     lambda: MathTex(
-        #         f"A({z.get_value()})={2 * fz(z.get_value() ** 2)}", color=GREEN
-        #     ),
-        # ).next_to(z_val_obj, DOWN, buff=0.5)
-
-        # area_val, z val, r val
-        # r_val_obj.move_to([0.0, 8.0, 4.0])
-        # z_val_obj.move_to([])
-        # r_val_obj.rotate(90 * DEGREES, [1.0, 0.0, 0.0])
-        # z_val_obj.rotate(90 * DEGREES, [1.0, 0.0, 0.0])
 
         self.move_camera(
             phi=75 * DEGREES,
@@ -290,20 +302,24 @@ class MainView(ThreeDScene):
             added_anims=[
                 FadeIn(y_axis, y_label),
                 Rotate(x_axis, -90 * DEGREES, [1.0, 0.0, 0.0]),
-                # FadeIn(r_val_obj, z_val_obj),
-                # Rotate(r_val_obj, 90 * DEGREES, [1.0, 0.0, 0.0]),
-                # Rotate(z_val_obj, 90 * DEGREES, [1.0, 0.0, 0.0])
             ],
             zoom=0.6,
         )
         self.wait()
         self.play(FadeIn(dot, r_line, r_obj, trace_lines, sqr_sec))
-        self.play(z.animate.set_value(Z_INT), run_time=3)
+        self.play(z.animate.set_value(Z_INT), run_time=15)
         self.wait()
 
 
 class TopView(Scene):
     def construct(self):
+        # ~~~~ RENDER TOGETHER, SPLICE AT MULTILINES ~~~~
+
+        """
+        To do this, let's say you start with a square like before.
+        Again, let's draw in a radius line.
+        """
+
         # scene setup
         sqr = Square(side_length=4.0).shift(UP * 0.5)
         sqr.rotate(45 * DEGREES)
@@ -314,38 +330,34 @@ class TopView(Scene):
         d_obj = MathTex("2r", color=RED).next_to(d_line, LEFT, buff=0.2)
 
         sqr_eqn = MathTex(
-            "A = \\frac{1}{2} r \\times 2r = 2r^{2}", color=PURPLE
+            "A = 2 \\times 2r \\times \\frac{1}{2} r = 2r^{2}", color=PURPLE
         ).next_to(sqr, DOWN, buff=0.2)
 
         self.play(FadeIn(sqr))
         self.play(Create(r_line), FadeIn(r_obj))
         self.wait()
+
+        """
+            If we draw in another 2 radii, we form 2 large right triangles.
+        """
+
         self.play(Create(d_line), FadeIn(d_obj))
         self.wait()
+
+        """
+            From this information, we can get the equation for a square based
+            on its radius by adding the area formed by the 2 triangles.
+            {go to MainView XZ}
+        """
+
         self.play(DrawBorderThenFill(sqr_eqn))
+        self.wait()
 
 
 class SideView(Scene):
     def construct(self):
-        """
-        We didn't mention how the function was defined at first, but it is z = -1.465x^2 + 1.02.
-        And in the YZ plane, it is z = -1.465y^2 + 1.02. We found this by finding the length of the
-        tent, which was 1.18 m. This was the hypotenuse of a 45-45-90 triangle. We decided to make the
-        corners of the tent's square base (or the legs of the triangle) our XY axes, so that meant
-        that the intercepts were all at plus or minus 1.18/sqrt(2).
+        # ~~~~ RENDER TOGETHER, SPLICE AT MULTILINES ~~~~
 
-        Based on these intercepts, our equations could be found by doing
-        f(x) = a * (x + 1.18/sqrt(2)) * (x - 1.18/sqrt(2)) and substituting the y-intercept (0, 1.02 m),
-        which is the height. The a value of -1.465 is then substituted back into this equation. Upon rearranging,
-        you get -1.465x^2 + 1.02.
-
-        In other words, that means z = f(x) = f(y). Since the graph of f(x) and f(y) are basically the same,
-        we'll only look at f(x). If we play the animation again, you can see that r = x, hence z is dependent on r.
-        {video ends}
-        Thus if we solve z = f(x) in terms of z, we'll get a function where r is dependent on z.
-        With this new function r = x = f(z), we are able to create our area function A(z) = 2 * r(z)^2.
-        {go to MainView}
-        """
         # scene setup
         axes = Axes(
             x_range=[-1, 1, 0.2], x_length=12, y_range=[0, 1.1, 0.1], y_length=5
@@ -424,23 +436,53 @@ class SideView(Scene):
             ).next_to(r, DOWN, buff=0.5)
         )
 
+        """
+            Here we're just going to redraw the graph real quick.
+            
+            We didn't mention how the function was defined at first, but it is z = -1.465x^2 + 1.02.
+            And in the YZ plane, it is z = -1.465y^2 + 1.02. We found this by finding the length of the
+            tent, which was 1.18 m. This was the hypotenuse of a 45-45-90 triangle. We decided to make the
+            corners of the tent's square base (or the legs of the triangle) our XY axes as shown in 3D before, 
+            so that meant that the intercepts were all at plus or minus 1.18/sqrt(2).
+
+            Based on these intercepts, our equations could be found by doing
+            f(x) = a * (x + 1.18/sqrt(2)) * (x - 1.18/sqrt(2)) and substituting the y-intercept (0, 1.02 m),
+            which is the height. The a value of -1.465 is then substituted back into this equation. Upon rearranging,
+            you get -1.465x^2 + 1.02. 
+        """
+
         self.play(DrawBorderThenFill(axes), run_time=2)
         self.play(DrawBorderThenFill(axes_labels))
         self.play(Create(cross_sec_graph), Write(func), run_time=2)
         self.play(Create(points, lag_ratio=0), run_time=3)
+        self.wait(2.5)
+
+        """
+            Since the graph of f(x) and f(y) are basically the same and z = f(x) = f(y),
+            we'll only look at f(x).
+        """
+
         self.wait()
         self.play(Create(dot), Create(lines), Create(r_line), Create(r_tex))
+        self.wait()
+
+        """
+            If we play the animation again, you can see that r = x, hence z is dependent on r.
+            {video ends, show slides}
+            Thus if we solve z = f(x) in terms of z, we'll get a function where r is dependent on z.
+            With this new function r = x = f(z), we are able to create our area function A(z) = 2 * r(z)^2.
+            {go to MainView Complete 3D Visual} 
+        """
+
+        self.wait()
         self.play(Create(r), Create(z))
         self.wait()
-        self.play(x.animate.set_value(0), run_time=10)
+        self.play(x.animate.set_value(0), run_time=15)
 
 
 class AreaGraph(MovingCameraScene):
     def construct(self):
-        """
-        Here is the graph of A(z). If we take the integral of A(z),
-        we get our area.
-        """
+        # ~~~~ RENDER TOGETHER, SPLICE AT MULTILINES ~~~~
 
         # scene setup
         axes = Axes(
@@ -478,9 +520,11 @@ class AreaGraph(MovingCameraScene):
             MathTex("A(z)", color=BLUE_A).scale(1.5).next_to(axes.c2p(0.3, 1.0, 0.0))
         )
         az_integral_label = (
-            MathTex("\int_0^{1.02} \! A(z) \, \mathrm{d}z \\approx 0.7102")
+            MathTex(
+                "V = \int_0^{1.02} \! A(z) \, \mathrm{d}z = \int_0^{1.02} \! \\frac{4}{293} \\times (51 - 50z) \, \mathrm{d}z \\approx 0.7102"
+            )
             .scale(1.2)
-            .next_to(axes.c2p(0.4, 1.0, 0.0))
+            .next_to(axes.c2p(0.35, 1.0, 0.0))
         )
 
         x_int = VGroup(
@@ -493,13 +537,33 @@ class AreaGraph(MovingCameraScene):
         self.camera.frame_center = axes.get_center()
         self.camera.frame.scale(2.0)
 
-        self.play(DrawBorderThenFill(axes), run_time=2)
+        """
+            So let's first look at the graph of r(z). It sort of looks like a rotated parabola as you'd expect.
+        """
+
+        self.play(DrawBorderThenFill(axes))
         self.play(DrawBorderThenFill(axes_labels))
         self.play(Create(fz_graph), run_time=2)
-        self.play(Create(fz_graph_label))
+        self.play(Create(fz_graph_label), run_time=0.5)
+        self.wait(2)
+
+        """
+            Here is the graph of A(z). Again, A(z) shows us the area of the tent's cross section at a given z value and 
+            is equal to 2 * r(z)^2.
+            Visually, it makes sense too, since the area is greater at a lower z, like near the base, and the area is
+            smaller at a higher z, like near the top of the tent.
+        """
+
+        self.wait()
         self.play(Create(az_graph), run_time=2)
-        self.play(Create(az_graph_label))
+        self.play(Create(az_graph_label), run_time=0.5)
         self.play(Write(x_int))
-        self.play(FadeIn(area), run_time=2)
+        self.wait()
+
+        """
+            Now if we take the integral of A(z), i.e. sum up the infinite amount of cross section areas, we get our volume.
+        """
+
+        self.play(FadeIn(area), run_time=5)
         self.play(TransformMatchingTex(az_graph_label, az_integral_label))
         self.wait()
