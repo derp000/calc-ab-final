3e602116377eea1f4a36011ea429e048f1fb5f5a
derp000 <84947701+derp000@users.noreply.github.com>
2023-06-05 15:44:53 -0400

    Basically done

--- project/scene.py
+++ project/scene.py
@@ -202,7 +202,7 @@ class MainView(ThreeDScene):
                 color=BLUE,
             ).next_to(r_val_obj, DOWN, buff=0.5)
         )
-        # self.add_fixed_orientation_mobjects(dot, r_line, r_obj, r_val_obj, z_val_obj)
+        self.add_fixed_orientation_mobjects(r_val_obj, z_val_obj)
         top_render = VGroup(dot, r_line, r_obj, r_val_obj, z_val_obj)
 
         self.stop_ambient_camera_rotation()
@@ -260,8 +260,8 @@ class MainView(ThreeDScene):
         self.next_section("Complete 3D Visualization")
 
         """
-            If we go back into 3D, we can see all these components in play. Notice how r is thus inversely
-            proportional to both z and A(z), the area of the purple square cross sections.
+            If we go back into 3D, we can see all these components in play. Notice that as the value of
+            z increases, r decreases and thus area decreases as well, which our new equation now models.
         """
 
         self.play(z.animate.set_value(0), run_time=0.1)
@@ -272,6 +272,17 @@ class MainView(ThreeDScene):
                 axes.c2p(fz(z.get_value()), 0, z.get_value())
             ),
         )
+        # area_obj = always_redraw(
+        #     lambda: MathTex(
+        #         f"A({z.get_value()})={2 * fz(z.get_value() ** 2)}", color=GREEN
+        #     ),
+        # ).next_to(z_val_obj, DOWN, buff=0.5)
+
+        # area_val, z val, r val
+        # r_val_obj.move_to([0.0, 8.0, 4.0])
+        # z_val_obj.move_to([])
+        # r_val_obj.rotate(90 * DEGREES, [1.0, 0.0, 0.0])
+        # z_val_obj.rotate(90 * DEGREES, [1.0, 0.0, 0.0])
 
         self.move_camera(
             phi=75 * DEGREES,
@@ -279,13 +290,16 @@ class MainView(ThreeDScene):
             added_anims=[
                 FadeIn(y_axis, y_label),
                 Rotate(x_axis, -90 * DEGREES, [1.0, 0.0, 0.0]),
+                # FadeIn(r_val_obj, z_val_obj),
+                # Rotate(r_val_obj, 90 * DEGREES, [1.0, 0.0, 0.0]),
+                # Rotate(z_val_obj, 90 * DEGREES, [1.0, 0.0, 0.0])
             ],
             zoom=0.6,
         )
         self.wait()
         self.play(FadeIn(dot, r_line, r_obj, trace_lines, sqr_sec))
-        self.play(z.animate.set_value(Z_INT), run_time=10)
-        self.wait(5)
+        self.play(z.animate.set_value(Z_INT), run_time=3)
+        self.wait()
 
 
 class TopView(Scene):
@@ -419,3 +433,73 @@ class SideView(Scene):
         self.play(Create(r), Create(z))
         self.wait()
         self.play(x.animate.set_value(0), run_time=10)
+
+
+class AreaGraph(MovingCameraScene):
+    def construct(self):
+        """
+        Here is the graph of A(z). If we take the integral of A(z),
+        we get our area.
+        """
+
+        # scene setup
+        axes = Axes(
+            x_range=[0, 1.1, 0.1],
+            x_length=12,
+            y_range=[0, 1.5, 0.2],
+            y_length=12,
+        )
+        axes.add_coordinates()
+
+        # graph setup
+        z_label = axes.get_x_axis_label("z")
+        fz_label = axes.get_y_axis_label("f(z)")
+        axes_labels = VGroup(z_label, fz_label)
+
+        z_label.move_to([6.5, -6.0, 0.0])
+        fz_label.move_to([-5.0, 5.5, 0.0])
+
+        # function setup
+        fz_graph = axes.plot(
+            lambda x: 2.0 * math.sqrt(51.0 - 50.0 * x) / math.sqrt(293),
+            x_range=[0, Z_INT],
+            color=RED_A,
+        )
+        az_graph = axes.plot(
+            lambda x: 2.0 * fz(float(x)) ** 2.0, x_range=[0, Z_INT], color=BLUE_A
+        )
+
+        fz_graph_label = (
+            MathTex("r(z)", color=RED_A)
+            .scale(1.5)
+            .next_to(axes.c2p(0.8, 0.4, 0.0), RIGHT, buff=0.4)
+        )
+        az_graph_label = (
+            MathTex("A(z)", color=BLUE_A).scale(1.5).next_to(axes.c2p(0.3, 1.0, 0.0))
+        )
+        az_integral_label = (
+            MathTex("\int_0^{1.02} \! A(z) \, \mathrm{d}z \\approx 0.7102")
+            .scale(1.2)
+            .next_to(axes.c2p(0.4, 1.0, 0.0))
+        )
+
+        x_int = VGroup(
+            Dot(point=axes.c2p(Z_INT, 0, 0), color=BLUE),
+            MathTex("(1.02, 0)").next_to(axes.c2p(Z_INT, 0, 0), UP, buff=0.2),
+        )
+
+        area = axes.get_area(graph=az_graph, x_range=[0, Z_INT], color=[BLUE, YELLOW])
+
+        self.camera.frame_center = axes.get_center()
+        self.camera.frame.scale(2.0)
+
+        self.play(DrawBorderThenFill(axes), run_time=2)
+        self.play(DrawBorderThenFill(axes_labels))
+        self.play(Create(fz_graph), run_time=2)
+        self.play(Create(fz_graph_label))
+        self.play(Create(az_graph), run_time=2)
+        self.play(Create(az_graph_label))
+        self.play(Write(x_int))
+        self.play(FadeIn(area), run_time=2)
+        self.play(TransformMatchingTex(az_graph_label, az_integral_label))
+        self.wait()
